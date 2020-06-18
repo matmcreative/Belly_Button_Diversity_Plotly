d99223aac789249ad46ce8877256d04caaaa707d
Lisa Reed Preston <lisareedpreston@Lisas-MacBook-Pro.local>
2020-06-18 15:58:58 -0500

    created metadata function

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,23 +1,44 @@
-d3.json("data/samples.json").then((data) => {
-    //  Create the Traces
-    var trace1 = {
-      x: data.otu_ids,
-      y: data.survival.map(val => Math.sqrt(val)),
-      type: "scatter",
-      name: "Belly Button Biodiversity"
-    };
+function metadataUpdate(sample) {
+    d3.json("data/samples.json").then((data) => {
+        const metadata = data.metadata;
+        // Filter the data for the object with the desired sample number
+        const resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        let result = resultArray[0];
+        
+        // Use d3 to select the panel with the id of #sample-metadata
+        let panel = d3.select('#sample-metadata');
+        console.log(panel)
+
+        // Use `.html("") to clear any existing metadata
+        panel.html("");
+
+        // Use `Object.entries` to add each key and value pair to the panel
+        Object.entries(result).forEach(([key, value]) => {
+        panel.append("h6").text(`${key.toUpperCase()}: ${value}`);
+        });
   
-    // Create the data array for the plot
-    var data = [trace1];
+    });
+  }
   
-    // Define the plot layout
-    var layout = {
-      title: "Square Root of Cancer Survival by Organ",
-      xaxis: { title: "Organ" },
-      yaxis: { title: "Square Root of Survival" }
-    };
-  
-    // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("plot", data, layout);
-  });
-  
+        //  Create the Traces
+        const trace1 = {
+        x: data.otu_ids,
+        y: data.survival.map(val => Math.sqrt(val)),
+        type: "scatter",
+        name: "Belly Button Biodiversity"
+        };
+    
+        // Create the data array for the plot
+        var data = [trace1];
+    
+        // Define the plot layout
+        var layout = {
+        title: "Square Root of Cancer Survival by Organ",
+        xaxis: { title: "Organ" },
+        yaxis: { title: "Square Root of Survival" }
+        };
+    
+        // Plot the chart to a div tag with id "plot"
+        Plotly.newPlot("plot", data, layout);
+    });
+};  
